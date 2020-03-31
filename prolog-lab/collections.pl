58546ed0f23d717af7170dbe84d32bc8a4d5ee73
Tom Koptel <tom.koptel@gmail.com>
2020-03-31 23:25:31 +0200

    Solution for insertInOrederedList

--- prolog-lab/collections.pl
+++ prolog-lab/collections.pl
@@ -18,3 +18,9 @@ decPair([], []).
 decPair([X0, X1 | L], [X0, X1_INCREMENTED | R]) :-
     X1_INCREMENTED is X1 - 1,
     decPair(L, R).
+
+insertInOrederedList(X, [], [X]).
+insertInOrederedList(X, [Y|Rest], [X,Y|Rest]) :-
+    X @< Y, !.
+insertInOrederedList(X, [Y|Rest0], [Y|Rest]) :-
+    insertInOrederedList(X, Rest0, Rest).
