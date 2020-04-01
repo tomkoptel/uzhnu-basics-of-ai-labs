9dbfcb5af83a1c134494b187a27668159693faca
Tom Koptel <tom.koptel@gmail.com>
2020-04-01 22:45:32 +0200

    Add ContainsComparison

--- src/main/kotlin/uzhnu/edu/bai/dialog/Comparison.kt
+++ src/main/kotlin/uzhnu/edu/bai/dialog/Comparison.kt
@@ -16,3 +16,17 @@ class ExactComparison(
         }
     }
 }
+
+class ContainsComparison(
+        private val sampleList: List<String>,
+        private val response: String
+) : Comparison {
+    override fun compare(string: String): String? {
+        val matches = sampleList.map { string.contains(it, ignoreCase = true) }.reduceRight { a, b -> a || b }
+        return if (matches) {
+            response
+        } else {
+            null
+        }
+    }
+}
