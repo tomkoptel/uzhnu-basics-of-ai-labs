1bc204f98f26623cbe73ac58667e18dbb7bb3c89
Tom Koptel <tom.koptel@gmail.com>
2020-04-08 22:44:31 +0200

    Fix ByRegEx.kt

--- src/main/kotlin/uzhnu/edu/bai/dialog/ByRegEx.kt
+++ src/main/kotlin/uzhnu/edu/bai/dialog/ByRegEx.kt
@@ -1,15 +1,17 @@
 package uzhnu.edu.bai.dialog
 
 class ByRegEx(
-        private val regEx: Regex,
-        private val response: String
+    private val regEx: Regex,
+    private val response: String
 ) : Comparison {
     override fun compare(string: String): String? {
         val matchResult = regEx.findAll(string).toList()
-        return if (matchResult.isEmpty()) {
-            null
+        return if (matchResult.isNotEmpty()) {
+            val result = matchResult.first()
+            val matching = result.groupValues.lastOrNull()
+            String.format(response, matching)
         } else {
-            response
+            null
         }
     }
 }
--- src/test/kotlin/uzhnu/edu/bai/dialog/ByRegExTest.kt
+++ src/test/kotlin/uzhnu/edu/bai/dialog/ByRegExTest.kt
@@ -2,14 +2,14 @@ package uzhnu.edu.bai.dialog
 
 import org.junit.Test
 
-
 internal class ByRegExTest {
     @Test
-    fun name() {
+    fun substitution() {
         val comparison = ByRegEx(
-                regEx = "hello (\\w+)".toRegex(),
-                response = "Hello, nice to see you"
+            regEx = "hello (\\w+)".toRegex(),
+            response = "Hello, %s nice to see you"
         )
-        print(comparison.compare("hello Kitty"))
+        val response = comparison.compare("hello Kitty")
+        assert(response == "Hello, Kitty nice to see you")
     }
 }
