c536147f3d9ff1f17092b8e732648e739a923b22
tomkoptel <tom.koptel@gmail.com>
2020-04-14 08:50:42 +0200

    Technology of distributed systems and parallel computing: Lab 3 (#8)

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,9 @@ dependencies {
     // Use the Kotlin JDK 8 standard library.
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
+    // We use coroutines for parallel computing lab
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.5")
+
     // Use the Kotlin test library.
     testImplementation("org.jetbrains.kotlin:kotlin-test")
 
