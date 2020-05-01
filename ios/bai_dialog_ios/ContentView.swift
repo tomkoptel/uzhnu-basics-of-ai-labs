1eb76806b460d4dbfced60a22ca154a204251368
Tom Koptel <tom.koptel@gmail.com>
2020-05-01 14:52:46 +0200

    Initial setup

---
+++ ios/bai_dialog_ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  bai_dialog_ios
+//
+//  Created by Tom Koptel on 01/05/2020.
+//  Copyright © 2020 Tom Koptel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
