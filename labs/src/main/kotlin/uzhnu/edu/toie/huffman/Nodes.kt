a9274f0b6f7e9a1c1e5af7a3c10bff2f71be14df
tomkoptel <tom.koptel@gmail.com>
2020-05-21 16:13:15 +0200

    Документація на Код Гаффмана (#13)

--- labs/src/main/kotlin/uzhnu/edu/toie/huffman/Nodes.kt
+++ labs/src/main/kotlin/uzhnu/edu/toie/huffman/Nodes.kt
@@ -2,13 +2,22 @@ package uzhnu.edu.toie.huffman
 
 import java.util.Comparator as JavaComparator
 
+/**
+ * Клас нода яка презентує один лист нашого дерева.
+ */
 sealed class Node(open val weight: Int) {
+    /**
+     * JavaComparator реалізує порівняня двух нодів в порядку від найменшого до найбільшого.
+     */
     object Ascending : JavaComparator<Node> {
         override fun compare(nodeA: Node, nodeB: Node): Int {
             return nodeA.weight.compareTo(nodeB.weight)
         }
     }
 
+    /**
+     * Об'єднує ноду з 'права += ліво' та створює батьківську ноду.
+     */
     operator fun plus(right: Node) : Node {
         val left = this
         return ParentNode(
@@ -19,11 +28,17 @@ sealed class Node(open val weight: Int) {
     }
 }
 
+/**
+ * Репрезентує ноду яка містить символ та вагу символа.
+ */
 data class ChildNode(
     val value: Char,
     override val weight: Int
 ) : Node(weight)
 
+/**
+ * Репрезентує ноду яка містить посилання дві ноди лівої та правої гілки.
+ */
 data class ParentNode(
     override val weight: Int,
     val left: Node,
